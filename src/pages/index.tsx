4a2088eb14e2834b5774fbaa885217db9531272b
Rikuto <orikutobasuke@gmail.com>
2023-04-24 13:55:29 +0900

    Chakra

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,12 @@
+import { Box, Flex, Text } from '@chakra-ui/react'
+import Link from 'next/link'
+
 export default function Index() {
   return (
-    <></>
+    <>
+      <Flex>
+        <Link href={'./samples/sample01.tsx'}>sample01</Link>
+      </Flex>
+    </>
   )
 }
--- src/pages/samples/sample01.tsx
+++ src/pages/samples/sample01.tsx
@@ -1,5 +1,11 @@
+import { Box, Flex, Text } from '@chakra-ui/react'
+
 export default function Sample01() {
     return (
-        <></>
+        <>
+            <Box>
+                <Text>Hello World</Text>
+            </Box>
+        </>
     )
 }
