0ffa80a504be4c2c4fd7fff0098eb1f08b0e93a0
Rikuto <orikutobasuke@gmail.com>
2023-04-24 14:34:16 +0900

    Chakra

--- src/pages/samples/01/index.tsx
+++ src/pages/samples/01/index.tsx
@@ -7,14 +7,20 @@ import {
     MenuGroup,
     MenuOptionGroup,
     MenuDivider,
+    IconButton,
 } from '@chakra-ui/react'
 
 export default function Sample01() {
     return (
         <>
-            <Flex>
+            <Flex w={'100%'} h={'100vh'} justifyContent={'center'} alignItems={'center'}>
                 <Menu>
-
+                    <MenuButton
+                        as={IconButton}
+                        aria-label='Option'
+                        variant={'outline'}
+                        bg={'blue.400'}
+                    ></MenuButton>
                     <MenuList>
                         <MenuItem>a</MenuItem>
                         <MenuItem>b</MenuItem>
