a55450feb5f6c8fae9ac4724ea114ea928ad75c2
Rikuto <orikutobasuke@gmail.com>
2023-04-28 12:19:23 +0900

    Chakra

--- src/pages/samples/EX-01/index.tsx
+++ src/pages/samples/EX-01/index.tsx
@@ -4,6 +4,10 @@ export default function EX01() {
     return (
         <>
             <Text>Chakra-UIについて</Text>
+            <Text as={'h1'}>Chakra-UIについて</Text>
+            <Text as={'h2'}>Chakra-UIについて</Text>
+            <Text as={'h3'}>Chakra-UIについて</Text>
+            <Text as={'h4'}>Chakra-UIについて</Text>
         </>
     )
 }
